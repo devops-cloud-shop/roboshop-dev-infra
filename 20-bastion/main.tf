ca2200faced6051e74877a0d6d8706ec57e64c72
Prav-Akula <pravs.akula@outlook.com>
2025-11-19 15:37:42 -0500

    roboshop-bastion

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "bastion" {
   ami                     = local.ami_id #mandatory
   instance_type           = "t3.micro" #mandatory
   vpc_security_group_ids  = [local.bastion_sg_id] #mandatory
+  subnet_id               = local.public_subnet_id
   tags                    = merge (
         local.common_tags,
         {
