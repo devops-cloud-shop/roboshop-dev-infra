192b84cc093cc98ed754d57bfc5844798ad30a04
Prav-Akula <pravs.akula@outlook.com>
2025-11-25 13:02:37 -0500

    db

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -19,9 +19,9 @@ data "aws_ami" "prav-devops" {
 }
 
 data "aws_ssm_parameter" "mongodb_sg_id" {
-  name = "${var.project_name}/${var.environment}/mongodb_sg_id"
+  name = "/${var.project_name}/${var.environment}/mongodb_sg_id"
 }
 
 data "aws_ssm_parameter" "database_subnet_ids" {
-  name = "${var.project_name}/${var.environment}/database_subnet_ids"
+  name = "/${var.project_name}/${var.environment}/database_subnet_ids"
 }
