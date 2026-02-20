6a651d1b29639d42aec3f7994e3de93fb5839e4c
Prav-Akula <pravs.akula@outlook.com>
2026-02-20 13:01:50 -0500

    dev-infra

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,6 +3,8 @@ resource "aws_instance" "bastion" {
   instance_type           = "t3.micro" #mandatory
   vpc_security_group_ids  = [local.bastion_sg_id] #mandatory
   subnet_id               = local.public_subnet_id
+
+  user_data = file("bastion.sh") #file is a funtn which reads bastion.sh and execute the script when bastion server is being started.
   tags                    = merge (
         local.common_tags,
         {
