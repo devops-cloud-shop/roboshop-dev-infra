2ac74793fa8ccd538aff4ec7c9c7609b58d0a7b9
Prav-Akula <pravs.akula@outlook.com>
2026-02-20 19:18:16 -0500

    dev-infra

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -22,6 +22,6 @@ data "aws_ssm_parameter" "catalogue_sg_id" {
   name = "/${var.project_name}/${var.environment}/catalogue_sg_id"
 }
 
-data "aws_ssm_parameter" "private_subnet_ids" {
-  name = "/${var.project_name}/${var.environment}/private_subnet_ids"
+data "aws_ssm_parameter" "private_subnet_id" {
+  name = "/${var.project_name}/${var.environment}/private_subnet_id"
 }
