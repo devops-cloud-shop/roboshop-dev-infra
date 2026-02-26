d3314f4f387c2db6daecd313a0c27311e76b5e6d
Prav-Akula <pravs.akula@outlook.com>
2026-02-26 13:14:26 -0500

    frontend LB update

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -19,7 +19,7 @@ resource "aws_lb" "frontend_alb" {
 resource "aws_lb_listener" "frontend_alb" {
   load_balancer_arn = aws_lb.frontend_alb.arn
   port              = "443"
-  protocol          = "HTTPs"
+  protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-Res-2021-06"
   certificate_arn   = local.frontend_alb_certificate_arn
   
