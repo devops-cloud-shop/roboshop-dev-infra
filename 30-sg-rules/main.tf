7aa9ab4381cfee1426b4b674285469275c257dad
Prav-Akula <pravs.akula@outlook.com>
2026-02-26 13:45:49 -0500

    sg-rules for frontend LB update

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -105,7 +105,7 @@ resource "aws_security_group_rule" "catalogue_backend_alb" {
 resource "aws_security_group_rule" "frontend_alb_public" {
   type              = "ingress"
   security_group_id = local.frontend_alb_sg_id
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
   from_port         = 443
   protocol          = "tcp"
   to_port           = 443
