40ed00b962e9904bea6181ed2bd1cd05f74f34df
Prav-Akula <pravs.akula@outlook.com>
2026-02-26 09:20:07 -0500

    catalogue

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -116,7 +116,7 @@ resource "aws_autoscaling_group" "catalogue" {
   desired_capacity          = 1
   force_delete              = false
 
-  vpc_zone_identifier       = [local.private_subnet_ids]
+  vpc_zone_identifier       = local.private_subnet_ids
   target_group_arns = [aws_lb_target_group.catalogue.arn]
 
   launch_template {
