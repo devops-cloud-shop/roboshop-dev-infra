bb2fa629a6e2e6c156002d9f6acff828d95f8cc0
Prav-Akula <pravs.akula@outlook.com>
2026-02-26 13:00:17 -0500

    acm and frontend update

---
+++ 80-frontend-alb/main.tf
@@ -0,0 +1,49 @@
+resource "aws_lb" "frontend_alb" {
+  name               = "${local.common_name_suffix}-frontend-alb" #roboshop-dev-backend-alb
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [local.frontend_alb_sg_id]
+  subnets            = local.public_subnet_ids
+
+  enable_deletion_protection = false  #true-prevents accidental deletion fron UI
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-frontend_alb"
+    }
+  )
+}
+
+#Backend ALB listening on port 80
+resource "aws_lb_listener" "frontend_alb" {
+  load_balancer_arn = aws_lb.frontend_alb.arn
+  port              = "443"
+  protocol          = "HTTPs"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-Res-2021-06"
+  certificate_arn   = local.frontend_alb_certificate_arn
+  
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/html"
+      message_body = "Hi, I'm from HTTPS frontend ALB"
+      status_code  = "200"
+    }
+  }
+}
+
+#route53 record for alb
+resource "aws_route53_record" "frontend_alb" {
+  zone_id = var.zone_id
+  name    = "${var.project_name}-${var.environment}.${var.domain_name}" #roboshop-dev.prav4cloud.online
+  type    = "A"
+
+  alias {
+    # These are ALB details, not our domain details
+    name                   = aws_lb.frontend_alb.dns_name
+    zone_id                = aws_lb.frontend_alb.zone_id
+    evaluate_target_health = true
+  }
+}  
