f74fea43d13fec6edd38686780e6a72f45813c3c
Prav-Akula <pravs.akula@outlook.com>
2026-02-27 16:49:25 -0500

    roboshop-infra

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,13 @@
-module "components" {
-    source = "../../terraform-aws-components"
-    component = var.component
-    rule_priority = var.rule_priority
+# module "components" {
+#     source = "../../terraform-aws-components"
+#     component = var.component
+#     rule_priority = var.rule_priority
   
+# }
+
+module "components" {
+  source = "git::https://github.com/devops-cloud-shop/terraform-roboshop-components.git?ref=main"
+  for_each = var.components
+  component = each.key
+  rule_priority = each.value.rule_priority
 }
